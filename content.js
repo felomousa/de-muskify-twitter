38ee9f360c14356db82faaf9e08fab68722a3e94
felomousa <felogmousa@gmail.com>
2025-04-14 03:16:10 -0700

    renamed doc + updated favicon

--- content.js
+++ content.js
@@ -18,7 +18,6 @@ const removeGrokDrawer = () => {
     document.querySelectorAll('[data-testid="GrokDrawer"]').forEach(el => el.remove());
 };
 
-
 // remove Grok actions button in each tweet
 const removeGrokActions = () => {
     document.querySelectorAll('button[aria-label="Grok actions"]').forEach(el => el.remove());
@@ -120,6 +119,31 @@ const replaceXWithTwitterBird = () => {
     }
 };
 
+// get rid of stupid x logo and rename back to twitter
+const updateFaviconAndTitle = () => {
+    const birdSvg = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="40" height="40">
+        <path fill="#03A9F4" d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162
+        c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,
+        33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67
+        c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,
+        1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0,0,0.022,0,0.086
+        c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135
+        c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104
+        C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945
+        C39.763,15.197,41.013,13.905,42,12.429"/>
+      </svg>`;
+
+    const svgDataUrl = "data:image/svg+xml," + encodeURIComponent(birdSvg);
+
+    document.querySelectorAll("link[rel*='icon']").forEach(el => el.remove());
+    const link = document.createElement("link");
+    link.rel = "icon";
+    link.href = svgDataUrl;
+    document.head.appendChild(link);
+
+    document.title = "demuskified twitter :)";
+};
 
 const removeUnwantedElements = () => {
     removeRemnants();
@@ -139,6 +163,7 @@ const removeUnwantedElements = () => {
     removeLists();
     replacePostWithTweet();
     removeWhiteGlowEffect();
+    updateFaviconAndTitle();
 };
 
 removeUnwantedElements();
